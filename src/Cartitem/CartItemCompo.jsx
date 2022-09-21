ed313dd86a2ac52e08920611d12af04d1068f2a3
Siddarth Singh <siddarthsingh2809@gmail.com>
2022-09-21 22:43:24 +0530

    Added Item Components

---
+++ src/Cartitem/CartItemCompo.jsx
@@ -0,0 +1,18 @@
+import { CartItemContainer, ItemDetails } from "./CartItemStyles";
+
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <CartItemContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <span>{name}</span>
+        <span>
+          {quantity} x ${price}
+        </span>
+      </ItemDetails>
+    </CartItemContainer>
+  );
+};
+
+export default CartItem;
