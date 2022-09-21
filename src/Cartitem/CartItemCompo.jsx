d0c257c63b088e197eda919b1886f6d47adaabbb
Siddarth Singh <siddarthsingh2809@gmail.com>
2022-09-21 22:31:40 +0530

    bug fixes

--- src/Cartitem/CartItemCompo.jsx
+++ src/Cartitem/CartItemCompo.jsx
@@ -3,15 +3,7 @@ import { CartItemContainer, ItemDetails } from "./CartItemStyles";
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <CartItemContainer>
-      <img src={imageUrl} alt={`${name}`} />
-      <ItemDetails>
-        <span>{name}</span>
-        <span>
-          {quantity} x ${price}
-        </span>
-      </ItemDetails>
-    </CartItemContainer>
+    
   );
 };
 
